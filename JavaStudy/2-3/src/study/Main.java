ab149fde069ce277e7f1283499c73ee90e8d8bf0
Yui-Dehara <99003079+Yui-Dehara@users.noreply.github.com>
2022-03-10 14:09:38 +0900

    Java 2章　オーバーロード・継承・オーバーライド　提出課題
    Java 2章　オーバーロード・継承・オーバーライド　提出課題

---
+++ JavaStudy/2-3/src/study/Main.java
@@ -0,0 +1,28 @@
+package study;
+
+/**
+*
+* 本課題では、継承・オーバーロードメソッドの基本的な使い方を学んでいきましょう。
+* 課題は問①から問③まであります。
+* 指定された値と変数名を守って記述してください。
+*
+* @author s.nanaumi
+*/
+public class Main {
+
+   public static void main(String[] args) {
+
+       // ③ Taskクラスのインスタンスを生成し、「doTask()」メソッドを呼び出しなさい。
+	   Task task = new Task();
+	   int taskValue = task.plus(10);
+	   System.out.println("plusメソッドの引数が一つの場合：" + taskValue);
+	   
+	   int taskMethod = task.plus(10, 20);
+	   System.out.println("plusメソッドの引数が二つの場合：" + taskMethod);
+	   
+	   int taskGroup = task.plus(10, 20, 30);
+	   System.out.println("plusメソッドの引数が三つの場合：" + taskGroup);
+	   
+	   }
+
+}
