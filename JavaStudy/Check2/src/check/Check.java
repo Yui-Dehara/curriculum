82bdc1961ee5ebdde17eadd5aa9eb3410ba2ac19
Yui-Dehara <99003079+Yui-Dehara@users.noreply.github.com>
2022-03-31 16:32:02 +0900

    Java2章　提出課題　2章 チェックテスト　再
    Java2章　提出課題　2章 チェックテスト　再

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,5 +1,7 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
 	/*【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
      *firstName →　自分の名字　lastName →【Check.java】にて自分の名前で初期化しなさい。
@@ -22,9 +24,9 @@ public class Check {
 		// TODO 自動生成されたメソッド・スタブ
 		Check.printName("唯", "出原");
 	//【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
-		Pet p = new Pet("java吉", "hoge");
+		Pet p = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		p.introduce(); 
-		RobotPet rP = new RobotPet("R2D2","ルーク");
+		RobotPet rP = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		rP.introduce();
 	}
 
